b304517c87223ac909e86223d4f94a5cc68a06d5
rhjoerg <rhjoerg@gmail.com>
2020-06-06 17:26:23 +0200

    2: Complete ModelRepository
    Task-Url: http://github.com/rhjoerg/rhj-embedded/issues/2

--- src/main/java/ch/rhj/embedded/maven/repository/ModelRepository.java
+++ src/main/java/ch/rhj/embedded/maven/repository/ModelRepository.java
@@ -1,6 +1,8 @@
 package ch.rhj.embedded.maven.repository;
 
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +15,8 @@ import org.apache.maven.model.io.ModelReader;
 @Named
 public class ModelRepository
 {
+	private final Map<String, ?> options = Map.of(ModelReader.IS_STRICT, true);
+
 	private final ModelReader reader;
 
 	private final Map<URL, Model> models = new HashMap<>();
@@ -21,5 +25,26 @@ public class ModelRepository
 	public ModelRepository(ModelReader reader)
 	{
 		this.reader = reader;
+	}
+
+	public synchronized Model get(URL url) throws Exception
+	{
+		Model model = models.get(url);
+
+		if (model == null)
+		{
+			try (InputStream input = url.openStream())
+			{
+				model = reader.read(input, options);
+				models.put(url, model);
+			}
+		}
+
+		return model;
+	}
+
+	public Model get(Path path) throws Exception
+	{
+		return get(path.toAbsolutePath().normalize().toUri().toURL());
 	}
 }
--- src/test/java/ch/rhj/embedded/maven/repository/ModelRepositoryTests.java
+++ src/test/java/ch/rhj/embedded/maven/repository/ModelRepositoryTests.java
@@ -1,6 +1,17 @@
 package ch.rhj.embedded.maven.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.inject.Inject;
+
+import org.apache.maven.model.Model;
+import org.codehaus.plexus.DefaultPlexusContainer;
+import org.junit.jupiter.api.Test;
 
 import ch.rhj.embedded.maven.WithMaven;
 
@@ -10,4 +21,25 @@ public class ModelRepositoryTests
 	@Inject
 	private ModelRepository modelRepository;
 
+	@Inject
+	private DefaultPlexusContainer container;
+
+	@Test
+	public void testCaching() throws Exception
+	{
+		Path path = Paths.get("pom.xml");
+		Model model1 = modelRepository.get(path);
+		Model model2 = modelRepository.get(path);
+
+		assertTrue(model1 == model2);
+	}
+
+	@Test
+	public void testEmbeddedModel() throws Exception
+	{
+		URL url = container.getContainerRealm().getResource("META-INF/maven/org.apache.maven/maven-embedder/pom.xml");
+		Model model = modelRepository.get(url);
+
+		assertEquals("maven-embedder", model.getArtifactId());
+	}
 }
